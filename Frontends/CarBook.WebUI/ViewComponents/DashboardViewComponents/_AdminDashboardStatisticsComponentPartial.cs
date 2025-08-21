129620787d40fefd3db5afa0ea56fd1137f331d3
Yusuf Ertürk <80910013+yusuferturkk@users.noreply.github.com>
2025-08-21 23:50:59 +0300

    Admin dashboard update.

---
+++ Frontends/CarBook.WebUI/ViewComponents/DashboardViewComponents/_AdminDashboardStatisticsComponentPartial.cs
@@ -0,0 +1,71 @@
+﻿using CarBook.Dto.StatisticDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBook.WebUI.ViewComponents.DashboardViewComponents
+{
+    public class _AdminDashboardStatisticsComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _AdminDashboardStatisticsComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            Random random = new Random();
+            var client = _httpClientFactory.CreateClient();
+
+            #region İstatistik1
+            var responseMessage = await client.GetAsync("https://localhost:7200/api/Statistics/GetCarCount");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                int carCountRandom = random.Next(0, 101);
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<ResultStatisticDto>(jsonData);
+                ViewBag.carCount = values?.carCount;
+                ViewBag.carCountRandom = carCountRandom;
+            }
+            #endregion
+
+            #region İstatistik2
+            var responseMessage2 = await client.GetAsync("https://localhost:7200/api/Statistics/GetAuthorCount");
+            if (responseMessage2.IsSuccessStatusCode)
+            {
+                int authorCountRandom = random.Next(0, 101);
+                var jsonData2 = await responseMessage2.Content.ReadAsStringAsync();
+                var values2 = JsonConvert.DeserializeObject<ResultStatisticDto>(jsonData2);
+                ViewBag.authorCount = values2?.authorCount;
+                ViewBag.authorCountRandom = authorCountRandom;
+            }
+            #endregion
+
+            #region İstatistik3
+            var responseMessage3 = await client.GetAsync("https://localhost:7200/api/Statistics/GetBlogCount");
+            if (responseMessage3.IsSuccessStatusCode)
+            {
+                int blogCountRandom = random.Next(0, 101);
+                var jsonData3 = await responseMessage3.Content.ReadAsStringAsync();
+                var values3 = JsonConvert.DeserializeObject<ResultStatisticDto>(jsonData3);
+                ViewBag.blogCount = values3?.blogCount;
+                ViewBag.blogCountRandom = blogCountRandom;
+            }
+            #endregion
+
+            #region İstatistik4
+            var responseMessage4 = await client.GetAsync("https://localhost:7200/api/Statistics/GetBrandCount");
+            if (responseMessage4.IsSuccessStatusCode)
+            {
+                int brandCountRandom = random.Next(0, 101);
+                var jsonData4 = await responseMessage4.Content.ReadAsStringAsync();
+                var values4 = JsonConvert.DeserializeObject<ResultStatisticDto>(jsonData4);
+                ViewBag.brandCount = values4?.brandCount;
+                ViewBag.brandCountRandom = brandCountRandom;
+            }
+            #endregion
+
+            return View();
+        }
+    }
+}
